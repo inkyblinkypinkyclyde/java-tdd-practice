6bc5ce25d45e907eddaf5e20d25326e6b94a81d0
inkyblinkypinkyclyde <42178384+inkyblinkypinkyclyde@users.noreply.github.com>
2022-08-01 17:05:42 +0100

    finished extensions

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -1,2 +1,30 @@
 public class Printer {
+    private String name;
+    private int paper;
+    private int toner;
+    public Printer(String name, int paper, int toner){
+        this.name = name;
+        this.paper = paper;
+        this.toner = toner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPaper() {
+        return paper;
+    }
+
+    public int getToner() {
+        return toner;
+    }
+
+    public void print(int pages, int sheets) {
+        int volumeTaken = pages*sheets;
+        if ((volumeTaken < paper) && (volumeTaken<toner)){
+            paper = paper - volumeTaken;
+            toner = toner - volumeTaken;
+        }
+    }
 }
--- src/test/java/PrinterTest.java
+++ src/test/java/PrinterTest.java
@@ -1,2 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PrinterTest {
+    Printer myPrinter;
+    @Before
+    public void beforeEach() {
+        myPrinter = new Printer("Mr. Grumpy", 100, 100);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Mr. Grumpy", myPrinter.getName());
+    }
+
+    @Test
+    public void hasPaper(){
+        assertEquals(100, myPrinter.getPaper());
+    }
+
+    @Test
+    public void hasToner() {
+        assertEquals(100, myPrinter.getToner());
+    }
+
+    @Test
+    public void canPrintIsAble(){
+        myPrinter.print(5, 5);
+        assertEquals(75, myPrinter.getPaper());
+        assertEquals(75, myPrinter.getToner());
+    }
+
+    @Test
+    public void canPrintIsNotAble(){
+        myPrinter.print(11, 11);
+        assertEquals(100, myPrinter.getPaper());
+        assertEquals(100, myPrinter.getToner());
+    }
 }
